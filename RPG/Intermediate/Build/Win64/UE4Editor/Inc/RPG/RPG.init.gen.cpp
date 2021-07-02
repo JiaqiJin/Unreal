49d04aef0e40a042eb1db165df2cf7c378ffc89c
kawakawaDe <fdsam@hotmail.com>
2021-07-02 11:21:10 +0100

    update

--- RPG/Intermediate/Build/Win64/UE4Editor/Inc/RPG/RPG.init.gen.cpp
+++ RPG/Intermediate/Build/Win64/UE4Editor/Inc/RPG/RPG.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeRPG_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x4E5F1528,
-				0x37E7D98D,
+				0x1B6E7577,
+				0xEF698D61,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
